6bc706a314e1cf7134d8854a087df9b6b464d02c
santhosh kumar <santhoshkumar.ssk3222@gmail.com>
2018-02-04 01:52:12 +0530

    database added

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -6,6 +6,51 @@ import json
 bootstrap=Bootstrap(app)
 @app.route("/")
 def home():
+	# This is the url to which the query is made
+	url = "https://data.annulment76.hasura-app.io/v1/query"
+
+	# This is the json payload for the query
+	requestPayload = {
+	    "type": "bulk",
+	    "args": [
+	        {
+	            "type": "run_sql",
+	            "args": {
+	                "sql": "select DISTINCT \"District\" from record;"
+	            }
+	        },
+	        {
+	            "type": "run_sql",
+	            "args": {
+	                "sql": "select DISTINCT \"Block\" from record;"
+	            }
+	        },
+	        {
+	            "type": "run_sql",
+	            "args": {
+	                "sql": "select DISTINCT \"Panchayat\" from record;"
+	            }
+	        },
+	        {
+	            "type": "run_sql",
+	            "args": {
+	                "sql": "select DISTINCT \"Village\" from record;"
+	            }
+	        }
+	    ]
+	}
+
+	# Setting headers
+	headers = {
+	    "Content-Type": "application/json",
+	    "Authorization": "Bearer 60bcda7f858ca2dd9d5cf503cdb7aeeafcc997f6c469c66a"
+	}
+
+	# Make the query and store response in resp
+	resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+	# resp.content contains the json response.
+	print resp.content
     return render_template("index.html")
 
 # Uncomment to add a new URL at /new
